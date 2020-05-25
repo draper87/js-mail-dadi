c32fc30a3e559dd9222968b749d26a4a6f3ca861
Oliver <draper87@gmail.com>
2020-05-25 17:02:49 +0200

    Fix

--- email/js/script.js
+++ email/js/script.js
@@ -5,17 +5,17 @@ var email = prompt('Inserisci indirizzo email');
 var archivioEmail = ['draper87@gmail.com', 'alessandro@libero.it', 'federico@yahoo.com', 'maria@tiscali.it'];
 
 // Creo una variabile di appoggio
-var emailVerificata = 0;
+var emailVerificata = false;
 
 
 // Verifico che l indirizzo email inserito dall utente rientri tra quelli autorizzati
 for (var i = 0; i < archivioEmail.length; i++) {
   if (archivioEmail[i] == email) {
-    emailVerificata = 1; // restituisco true se la mail viene verificata
+    emailVerificata = true; // restituisco true se la mail viene verificata
   }
 }
 // In base al valore di emailVerificata andiamo a restituire un messaggio
-if (emailVerificata == 0) {
+if (emailVerificata == false) {
   document.getElementById('autorizzato').innerHTML = 'Non sei autorizzato ad accedere alla pagina';
 }
 else {
