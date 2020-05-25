4119455898d5484e9ea6f99f9e6379816d5935a8
Oliver <draper87@gmail.com>
2020-05-25 16:25:26 +0200

    Fix

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,19 +2,19 @@
 // Creiamo un gioco dei dadi dove vince chi ha il punteggio piu alto da 1 a 6
 
 // Generiamo lanci casuali usando la funzione math.random
-var dadiGiocatore = Math.floor(Math.random() * 6) + 1;
-var dadiComputer = Math.floor(Math.random() * 6) + 1;
+var giocatore = Math.floor(Math.random() * 6) + 1;
+var computer = Math.floor(Math.random() * 6) + 1;
 
 // Mi creo le variabili per accedere al DOM
 var risultato = document.getElementById('risultato');
-document.getElementById('dadigiocatore').innerHTML = 'Hai lanciato ' + dadiGiocatore;
-document.getElementById('dadicomputer').innerHTML = 'Il computer ha lanciato ' + dadiComputer;
+document.getElementById('dadigiocatore').innerHTML = 'Hai lanciato ' + giocatore;
+document.getElementById('dadicomputer').innerHTML = 'Il computer ha lanciato ' + computer;
 
 // Verifico i due lanci per determinare chi ha vinto
-if ( dadiGiocatore == dadiComputer) {
+if ( giocatore == computer) {
   risultato.innerHTML = 'Parità!';
 }
-else if (dadiGiocatore > dadiComputer) {
+else if (giocatore > computer) {
   risultato.innerHTML = 'Hai vinto!';
 }
 else {
